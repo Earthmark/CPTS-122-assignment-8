9d961bd3159e83624db48a7b6809b3fc26fe8389
Daniel Miller <earthmark.miller@gmail.com>
2012-04-09 21:56:06 -0700

    Changed some file names, also added a summary for each attribute.

--- Assignment 8/Assignment 8/Assignment 8/Classes/Car.cs
+++ Assignment 8/Assignment 8/Assignment 8/Classes/Car.cs
@@ -12,6 +12,9 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Assignment_8
 {
+    /// <summary>
+    /// Controls a Car's attributes and model
+    /// </summary>
     enum CarType : byte
     {
         SmallCar,
@@ -22,23 +25,47 @@ namespace Assignment_8
     class Car
     {
         /*Base Attributes*/
-        //car sprite
-        public Texture2D carSkin { get; private set; }
-        //car position
-        public Vector2 position { get; set; }
-        //car angle in theta
-        public float angle { get; set; }
-        //true if player controled, false if NPC
-        public bool isHuman { get; set; }
-        //current car speed
-        public float curSpeed { get; set; }
+            /// <summary>
+            /// Car texture attribute.
+            /// </summary>
+            public Texture2D skin { get; set; }
+
+            /// <summary>
+            /// The Car vector position in the track.
+            /// </summary>
+            public Vector2 position { get; set; }
+
+            /// <summary>
+            /// The Car angle in theta, use math libraries to work out vectors.
+            /// </summary>
+            public float angle { get; set; }
+
+            /// <summary>
+            /// Controls if the Car is NPC or Player driven.
+            /// </summary>
+            public bool isHuman { get; set; }
+
+            /// <summary>
+            /// The Car's current speed, changes often.
+            /// </summary>
+            public float currentSpeed { get; set; }
 
         /*Stats, changed by changing the type*/
-        public float topSpeed { get; private set; }
-        public float turningSpeed { get; private set; }
-        public float accel { get; private set; }
-        public float mass { get; private set; }
-        //master setter for above values
+            /// <summary>
+            /// Nearly static attribute of top speed.
+            /// </summary>
+            public static float topSpeed { get; private set; }
+            /// <summary>
+            /// 
+            /// </summary>
+            public static float turningSpeed { get; private set; }
+            public static float accel { get; private set; }
+            public float mass { get; private set; }
+            //master setter for above values.
+
+        /// <summary>
+        /// Changes car attributes based on what is chosen.
+        /// </summary>
         public CarType carType
         {
             get
@@ -75,16 +102,35 @@ namespace Assignment_8
             }
         }
 
-        //sets car skin, used in texture initalization
-        public void setSkin(Texture2D carTexture)
+        /// <summary>
+        /// Uses the carType attribute to find correct texture string.
+        /// </summary>
+        /// <returns>Texture ID string</returns>
+        public string modelString()
         {
-            carSkin = carTexture;
+            switch (carType)
+            {
+                case CarType.Semi:
+                    return "Semi";
+
+                case CarType.SmallCar:
+                    return "SmallCar";
+
+                case CarType.Truck:
+                    return "Truck";
+
+                default:
+                    throw new ArgumentOutOfRangeException ("carType contained an unknown value")
+            }
         }
 
-        //used to set car origin position, helpful so rotation works correctly
-        public Vector2 getCarOrigin()
+        /// <summary>
+        /// Corrects the rotation offset
+        /// </summary>
+        /// <returns>Center point of the car</returns>
+        public Vector2 carOrigin()
         {
-            return new Vector2(carSkin.Width / 2, carSkin.Height / 2);
+            return new Vector2(skin.Width / 2, skin.Height / 2);
         }
     }
 }
--- Assignment 8/Assignment 8/Assignment 8/GameTrack.cs
+++ Assignment 8/Assignment 8/Assignment 8/GameTrack.cs
@@ -14,12 +14,12 @@ namespace Assignment_8
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class GameTrack : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public GameTrack()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
