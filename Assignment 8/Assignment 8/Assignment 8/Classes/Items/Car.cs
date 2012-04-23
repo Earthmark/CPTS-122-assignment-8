65dbf8ee8f488351994b9e8edc6275a482805e73
Daniel Miller <earthmark.miller@gmail.com>
2012-04-22 20:28:11 -0700

    Added update function, also some issues with collisions in the commit required this to be added manually.

--- Assignment 8/Assignment 8/Assignment 8/Classes/Items/Car.cs
+++ Assignment 8/Assignment 8/Assignment 8/Classes/Items/Car.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,42 +27,42 @@ namespace Assignment_8
         /// <summary>
         /// The Car vector position in the track.
         /// </summary>
-        public Vector2 position { get; set; }
+        public Vector2 position;
 
         /// <summary>
         /// The Car angle in theta, use math libraries to work out vectors.
         /// </summary>
-        public float angle { get; set; }
+        public float angle;
 
         /// <summary>
         /// Controls if the Car is NPC or Player driven.
         /// </summary>
-        public bool isHuman { get; set; }
+        public bool isHuman;
 
         /// <summary>
         /// The Car's current speed, changes often.
         /// </summary>
-        public float currentSpeed { get; set; }
+        public float currentSpeed;
 
-        private CarTurnState turnState { get; set; }
+        private CarTurnState turnState;
 
         /*Stats, changed by changing the type*/
-        private static float topSpeed { get; set; }
-        private static float turningSpeed { get; set; }
-        private static float accel { get; set; }
-        private static float mass { get; set; }
+        private static float topSpeed;
+        private static float turningSpeed;
+        private static float accel;
+        private static float mass;
+        private static float friction;
 
         /*Car models*/
         private static List<List<Texture2D>> allCars;
 
         /*Used to make drawing faster*/
-        private static Rectangle source { get; set; }
-        private static Vector2 carOrigin { get; set; }
-        private static Rectangle dest { get; set; }
-        private static float scale { get; set; }
+        private static Rectangle source;
+        private static Vector2 carOrigin;
+        private static float scale;
 
-        private static int totalCarNum { get; set; }
-        private int carNum { get; set; }
+        private static int totalCarNum;
+        private int carNum;
         #endregion
 
         #region Constructor
@@ -102,6 +102,12 @@ namespace Assignment_8
             source = new Rectangle(0, 0, skin.Width, skin.Height);
             carOrigin = new Vector2(skin.Width / 2.0f, skin.Height / 2.0f);
             scale = 1.0f;
+            
+            topSpeed = 10.0f;
+            turningSpeed = 0.1f;
+            accel = 0.5f;
+            mass = 10.0f;
+            friction = 0.1f;
         }
 
         public Car ()
@@ -123,6 +129,80 @@ namespace Assignment_8
         {
             Engine.SpriteBatch.Draw(allCars[carNum][(int)turnState], position, source, Color.White, 
                 angle, carOrigin, scale, SpriteEffects.None, 0);
+        }
+        #endregion
+
+        #region Update
+        public void update(KeyboardState keyState)
+        {
+            if (isHuman)
+            {
+                humanUpdate(keyState);
+            }
+            else
+            {
+                computerUpdate();
+            }
+
+            positionUpdate();
+        }
+
+        private void humanUpdate(KeyboardState keyState)
+        {
+            //turning
+            turnState = CarTurnState.Straight;
+
+            if (keyState.IsKeyDown(Keys.Left))
+            {
+                angle -= turningSpeed;
+
+                turnState = CarTurnState.Left;
+            }
+            if (keyState.IsKeyDown(Keys.Right))
+            {
+                angle += turningSpeed;
+
+                turnState = CarTurnState.Right;
+            }
+            //acceleration
+            if (keyState.IsKeyDown(Keys.Up) && currentSpeed < topSpeed)
+            {
+                currentSpeed += accel;
+
+                if (currentSpeed > topSpeed)
+                {
+                    currentSpeed = topSpeed;
+                }
+            }
+            else if (currentSpeed > 0)
+            {
+                currentSpeed -= friction;
+            }
+
+            if (keyState.IsKeyDown(Keys.Down) && currentSpeed > -topSpeed)
+            {
+                currentSpeed -= accel;
+
+                if (currentSpeed < -topSpeed)
+                {
+                    currentSpeed = -topSpeed;
+                }
+            }
+            else if (currentSpeed < 0)
+            {
+                currentSpeed += friction;
+            }
+        }
+
+        private void computerUpdate()
+        {
+
+        }
+
+        private void positionUpdate()
+        {
+            position.X += (float)(currentSpeed * Math.Cos(angle));
+            position.Y += (float)(currentSpeed * Math.Sin(angle));
         }
         #endregion
     }
